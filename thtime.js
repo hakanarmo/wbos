5627f97f78252c27ae4c06df4d5d49ac34a9d1d9
aringo <37981873+hakanarmo@users.noreply.github.com>
2018-08-16 22:33:54 +0200

    Update thtime.js

--- thtime.js
+++ thtime.js
@@ -1,18 +1,18 @@
 module.exports = function (){
     currentUtcTime = new Date(); // This is in UTC
-    thTimeZone = new Date(currentUtcTime.toLocaleString('en-US', { timeZone: 'Turkey/Gmt+3' }));
+    trTimeZone = new Date(currentUtcTime.toLocaleString('en-US', { timeZone: 'Turkey/Gmt+3' }));
     days = new Array('week', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','week')
-    day = thTimeZone.getDay()
+    day = trTimeZone.getDay()
     return{
-        day : thTimeZone.getDay(),
+        day : trTimeZone.getDay(),
         days : days[day],
         dayz : days[day+1],
-        d : thTimeZone.getDate(),
-        month : thTimeZone.getMonth(),
-        year : thTimeZone.getFullYear(),
-        h : thTimeZone.getHours(),
-        m : thTimeZone.getMinutes(),  
-        s : thTimeZone.getSeconds()
+        d : trTimeZone.getDate(),
+        month : trTimeZone.getMonth(),
+        year : trTimeZone.getFullYear(),
+        h : trTimeZone.getHours(),
+        m : trTimeZone.getMinutes(),  
+        s : trTimeZone.getSeconds()
 
          }
     }
