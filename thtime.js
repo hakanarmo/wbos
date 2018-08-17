eb7a0fac91a7f4dcf1e47f876fe0578d65cade01
aringo <37981873+hakanarmo@users.noreply.github.com>
2018-08-17 05:19:09 +0200

    Update thtime.js

--- thtime.js
+++ thtime.js
@@ -1,6 +1,6 @@
 module.exports = function (){
     currentUtcTime = new Date(); // This is in UTC
-    thTimeZone = new Date(currentUtcTime.toLocaleString('en-US', { timeZone: 'Asia/Bangkok' }));
+    thTimeZone = new Date(currentUtcTime.toLocaleString('UTC'));
     days = new Array('week', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','week')
     day = thTimeZone.getDay()
     return{
